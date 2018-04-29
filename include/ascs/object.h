0e17ec1c21d420b9c305e133049c6e9f5da668d0
wolf <wolf@debian-wolf>
2018-04-29 19:45:00 +0800

    Fix bug: the hook for asynchronous calling cannot work properly with low compilers.

--- include/ascs/object.h
+++ include/ascs/object.h
@@ -32,43 +32,43 @@ public:
 
 #if (defined(_MSC_VER) && _MSC_VER > 1800) || (defined(__cplusplus) && __cplusplus > 201103L)
 	#if ASIO_VERSION >= 101100
-	template<typename F> void post(F&& handler) {asio::post(io_context_, [unused(this->async_call_indicator), handler(std::move(handler))]() {handler();});}
-	template<typename F> void post(const F& handler) {asio::post(io_context_, [unused(this->async_call_indicator), handler]() {handler();});}
+	template<typename F> void post(F&& handler) {asio::post(io_context_, [ref_holder(this->aci), handler(std::move(handler))]() {handler();});}
+	template<typename F> void post(const F& handler) {asio::post(io_context_, [ref_holder(this->aci), handler]() {handler();});}
 	//Don't use defer function unless you fully understand asio::defer, which was introduced in asio 1.11
-	template<typename F> void defer(F&& handler) {asio::defer(io_context_, [unused(this->async_call_indicator), handler(std::move(handler))]() {handler();});}
-	template<typename F> void defer(const F& handler) {asio::defer(io_context_, [unused(this->async_call_indicator), handler]() {handler();});}
+	template<typename F> void defer(F&& handler) {asio::defer(io_context_, [ref_holder(this->aci), handler(std::move(handler))]() {handler();});}
+	template<typename F> void defer(const F& handler) {asio::defer(io_context_, [ref_holder(this->aci), handler]() {handler();});}
 	#else
-	template<typename F> void post(F&& handler) {io_context_.post([unused(this->async_call_indicator), handler(std::move(handler))]() {handler();});}
-	template<typename F> void post(const F& handler) {io_context_.post([unused(this->async_call_indicator), handler]() {handler();});}
+	template<typename F> void post(F&& handler) {io_context_.post([ref_holder(this->aci), handler(std::move(handler))]() {handler();});}
+	template<typename F> void post(const F& handler) {io_context_.post([ref_holder(this->aci), handler]() {handler();});}
 	#endif
 
-	template<typename F> handler_with_error make_handler_error(F&& handler) const {return [unused(this->async_call_indicator), handler(std::move(handler))](const auto& ec) {handler(ec);};}
-	template<typename F> handler_with_error make_handler_error(const F& handler) const {return [unused(this->async_call_indicator), handler](const auto& ec) {handler(ec);};}
+	template<typename F> handler_with_error make_handler_error(F&& handler) const {return [ref_holder(this->aci), handler(std::move(handler))](const auto& ec) {handler(ec);};}
+	template<typename F> handler_with_error make_handler_error(const F& handler) const {return [ref_holder(this->aci), handler](const auto& ec) {handler(ec);};}
 
 	template<typename F> handler_with_error_size make_handler_error_size(F&& handler) const
-		{return [unused(this->async_call_indicator), handler(std::move(handler))](const auto& ec, auto bytes_transferred) {handler(ec, bytes_transferred);};}
+		{return [ref_holder(this->aci), handler(std::move(handler))](const auto& ec, auto bytes_transferred) {handler(ec, bytes_transferred);};}
 	template<typename F> handler_with_error_size make_handler_error_size(const F& handler) const
-		{return [unused(this->async_call_indicator), handler](const auto& ec, auto bytes_transferred) {handler(ec, bytes_transferred);};}
+		{return [ref_holder(this->aci), handler](const auto& ec, auto bytes_transferred) {handler(ec, bytes_transferred);};}
 #else
 	#if ASIO_VERSION >= 101100
-	template<typename F> void post(const F& handler) {auto unused(async_call_indicator); asio::post(io_context_, [=]() {handler();});}
+	template<typename F> void post(const F& handler) {auto ref_holder(aci); asio::post(io_context_, [=]() {(void) ref_holder; handler();});}
 	//Don't use defer function unless you fully understand asio::defer, which was introduced in asio 1.11
-	template<typename F> void defer(const F& handler) {auto unused(async_call_indicator); asio::defer(io_context_, [=]() {handler();});}
+	template<typename F> void defer(const F& handler) {auto ref_holder(aci); asio::defer(io_context_, [=]() {(void) ref_holder; handler();});}
 	#else
-	template<typename F> void post(const F& handler) {auto unused(async_call_indicator); io_context_.post([=]() {handler();});}
+	template<typename F> void post(const F& handler) {auto ref_holder(aci); io_context_.post([=]() {(void) ref_holder; handler();});}
 	#endif
-	template<typename F> handler_with_error make_handler_error(const F& handler) const {auto unused(async_call_indicator); return [=](const asio::error_code& ec) {handler(ec);};}
+	template<typename F> handler_with_error make_handler_error(const F& handler) const {auto ref_holder(aci); return [=](const asio::error_code& ec) {(void) ref_holder; handler(ec);};}
 	template<typename F> handler_with_error_size make_handler_error_size(const F& handler) const
-		{auto unused(async_call_indicator); return [=](const asio::error_code& ec, size_t bytes_transferred) {handler(ec, bytes_transferred);};}
+		{auto ref_holder(aci); return [=](const asio::error_code& ec, size_t bytes_transferred) {(void) ref_holder; handler(ec, bytes_transferred);};}
 #endif
 
-	bool is_async_calling() const {return !async_call_indicator.unique();}
-	bool is_last_async_call() const {return async_call_indicator.use_count() <= 2;} //can only be called in callbacks
+	bool is_async_calling() const {return !aci.unique();}
+	bool is_last_async_call() const {return aci.use_count() <= 2;} //can only be called in callbacks
 	inline void set_async_calling(bool) {}
 
 protected:
-	object(asio::io_context& _io_context_) : async_call_indicator(std::make_shared<char>('\0')), io_context_(_io_context_) {}
-	std::shared_ptr<char> async_call_indicator;
+	object(asio::io_context& _io_context_) : aci(std::make_shared<char>('\0')), io_context_(_io_context_) {}
+	std::shared_ptr<char> aci; //asynchronous calling indicator
 #else
 	#if ASIO_VERSION >= 101100
 	template<typename F> void post(F&& handler) {asio::post(io_context_, std::move(handler));}
@@ -87,13 +87,13 @@ protected:
 	template<typename F> inline F&& make_handler_error_size(F&& f) const {return std::move(f);}
 	template<typename F> inline const F& make_handler_error_size(const F& f) const {return f;}
 
-	inline bool is_async_calling() const {return async_calling;}
+	inline bool is_async_calling() const {return aci;}
 	inline bool is_last_async_call() const {return true;}
-	inline void set_async_calling(bool value) {async_calling = value;}
+	inline void set_async_calling(bool value) {aci = value;}
 
 protected:
-	object(asio::io_context& _io_context_) : async_calling(false), io_context_(_io_context_) {}
-	bool async_calling;
+	object(asio::io_context& _io_context_) : aci(false), io_context_(_io_context_) {}
+	bool aci; //asynchronous calling indicator
 #endif
 
 	asio::io_context& io_context_;
