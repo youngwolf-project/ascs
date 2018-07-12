42363d4bfa8af67c5979b5b5fd517a49e20486c6
youngowlf <mail2tao@163.com>
2018-07-12 16:48:04 +0800

    Optimize build script for mingw.

--- examples/mingw-build.bat
+++ examples/mingw-build.bat
@@ -1,27 +1,14 @@
+@echo off
 set cflag=-Wall -fexceptions -std=c++17 -pthread -O2 -DNDEBUG -DASIO_STANDALONE -DASIO_NO_DEPRECATED -I../../../asio/asio/include -I../../include/
 set lflag=-pthread -s -lstdc++ -lws2_32 -lwsock32
 
-cd client
-g++ %cflag% client.cpp -o client.exe %lflag%
-cd ../echo_client
-g++ %cflag% echo_client.cpp -o echo_client.exe %lflag%
-cd ../echo_server
-g++ %cflag% echo_server.cpp -o echo_server.exe %lflag%
-cd ../concurrent_client
-g++ %cflag% concurrent_client.cpp -o concurrent_client.exe %lflag%
-cd ../concurrent_server
-g++ %cflag% concurrent_server.cpp -o concurrent_server.exe %lflag%
-cd ../pingpong_client
-g++ %cflag% pingpong_client.cpp -o pingpong_client.exe %lflag%
-cd ../pingpong_server
-g++ %cflag% pingpong_server.cpp -o pingpong_server.exe %lflag%
-cd ../file_client
-g++ %cflag% file_client.cpp -o file_client.exe %lflag%
-cd ../file_server
-g++ %cflag% file_socket.cpp file_server.cpp -o file_server.exe %lflag%
-cd ../udp_test
-g++ %cflag% udp_test.cpp -o udp_test.exe %lflag%
-cd ../ssl_test
-g++ %cflag% ssl_test.cpp -o ssl_test.exe %lflag%
-cd ..
+for /d %%i in (*) do call:build %%i
+goto:eof
 
+:build
+@echo on
+cd %~1
+g++ %cflag% *.cpp -o %~1.exe %lflag%
+cd ..
+@echo off
+goto:eof
