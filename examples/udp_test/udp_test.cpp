ff10f2232208eb5a6cfe5f0d4d1d9741cb53a5af
youngwolf <mail2tao@163.com>
2018-09-28 16:43:49 +0800

    Use future instead of condition_variable for sync message sending.

--- examples/udp_test/udp_test.cpp
+++ examples/udp_test/udp_test.cpp
@@ -27,16 +27,16 @@ std::thread create_sync_recv_thread(single_service& service)
 {
 	return std::thread([&service]() {
 		std::list<single_service::out_msg_type> msg_can;
-		single_service::sync_call_result re = single_service::sync_call_result::SUCCESS;
+		sync_call_result re = sync_call_result::SUCCESS;
 		do
 		{
 			re = service.sync_recv_msg(msg_can, 50); //ascs will not maintain messages in msg_can anymore after sync_recv_msg return, please note.
-			if (single_service::sync_call_result::SUCCESS == re)
+			if (sync_call_result::SUCCESS == re)
 			{
 				do_something_to_all(msg_can, [](single_service::out_msg_type& msg) {printf("sync recv(" ASCS_SF ") : %s\n", msg.size(), msg.data());});
 				msg_can.clear(); //sync_recv_msg just append new message(s) to msg_can, please note.
 			}
-		} while (single_service::sync_call_result::SUCCESS == re || single_service::sync_call_result::TIMEOUT == re);
+		} while (sync_call_result::SUCCESS == re || sync_call_result::TIMEOUT == re);
 		puts("sync recv end.");
 	});
 }
