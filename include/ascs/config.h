a3d4f024c4a26535cc4177943ad432e8cb1eef06
yang li <li.10.yang@nokia.com>
2019-02-21 17:10:12 +0800

    Move virtual function on_create from object_pool to ascs::socket.

--- include/ascs/config.h
+++ include/ascs/config.h
@@ -478,6 +478,7 @@
  * Limit send and recv buffers by acctual utilization (in byte) rather than message number before, so macro ASCS_MAX_MSG_NUM been renamed to
  *  ASCS_MAX_SEND_BUF and ASCS_MAX_RECV_BUF, and unit been changed to byte.
  * statistic.send_msg_sum may be bigger than before (but statistic.send_byte_sum will be the same), see ENHANCEMENTS section for more details.
+ * Virtual function on_create has been moved from object_pool to ascs::socket.
  *
  * HIGHLIGHT:
  * Make client_socket_base be able to call multi_client_base (via i_matrix) like server_socket_base call server_base (via i_server),
@@ -500,6 +501,10 @@
  * Control send and recv buffer accurately rather than just message number before, see macro ASCS_MAX_SEND_BUF and ASCS_MAX_RECV_BUF for more details.
  * direct_send_msg and direct_sync_send_msg support batch operation.
  * Introduce virtual function type_name() and type_id() to ascs::socket, they can identify whether a given two ascs::socket has the same type.
+ * force_shutdown and graceful_shutdown support reconnecting even if the link has broken.
+ * Virtual function on_create has been moved from object_pool to ascs::socket, if you want to do some initialization for this socket, do it in on_create
+ *  rather than the constructor, this is because if this socket is reused from the object pool, its constructor will not be called.
+ *  for tcp::single_client_base, ssl::single_client_base and udp::single_service_base, this virtual function will never be called, please note.
  *
  * DELETION:
  *
