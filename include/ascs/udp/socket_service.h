c9dcce3d7ecfa95201c14f02bb4b1100b6e16cf7
yang li <li.10.yang@nokia.com>
2019-04-30 17:18:23 +0800

    Drop the 'reset' parameter in multi_socket_service::add_socket, this means never call reset() for the socket anymore. Don't call reset() in ascs::single_socket_service::init() and ascs::multi_socket_service::init() anymore.

--- include/ascs/udp/socket_service.h
+++ include/ascs/udp/socket_service.h
@@ -39,7 +39,7 @@ public:
 			return socket_ptr;
 
 		socket_ptr->set_local_addr(port, ip);
-		return this->add_socket(socket_ptr) ? socket_ptr : typename Pool::object_type();
+		return add_socket(socket_ptr) ? socket_ptr : typename Pool::object_type();
 	}
 	typename Pool::object_type add_socket(unsigned short port, unsigned short peer_port, const std::string& ip = std::string(), const std::string& peer_ip = std::string())
 	{
@@ -49,7 +49,7 @@ public:
 
 		socket_ptr->set_local_addr(port, ip);
 		socket_ptr->set_peer_addr(peer_port, peer_ip);
-		return this->add_socket(socket_ptr) ? socket_ptr : typename Pool::object_type();
+		return add_socket(socket_ptr) ? socket_ptr : typename Pool::object_type();
 	}
 
 	//functions with a socket_ptr parameter will remove the link from object pool first, then call corresponding function
