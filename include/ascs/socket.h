9171f951deb78dab2c8edd947e5b8e44b21155aa
youngwolf <li.10.yang@nokia.com>
2019-12-12 11:29:35 +0800

    Eliminate an needless assert in socket::do_start. Fix bug: unpacker may never be able to parse any more messages correctly after reconnecting to the server. The root cause is, some legacy data (not a whole message) may left behind in unpacker's internal buffer. User can also fix this issue by calling the unpacker's reset interface in on_recv_error, on_close, after_close or on_connect. This fix needs unpacker's support, the framework just invoke the unpacker's reset interface. It's a wrong enhancement introduced by version 1.2.

--- include/ascs/socket.h
+++ include/ascs/socket.h
@@ -253,7 +253,6 @@ protected:
 #if ASCS_HEARTBEAT_INTERVAL > 0
 		start_heartbeat(ASCS_HEARTBEAT_INTERVAL);
 #endif
-		assert(is_ready());
 		send_msg(); //send buffer may have msgs, send them
 		recv_msg();
 
@@ -334,6 +333,7 @@ protected:
 
 		if (stopped())
 		{
+			unpacker_->reset(); //very important, otherwise, the unpacker will never be able to parse any more messages if its buffer has legacy data
 			on_close();
 			after_close();
 		}
@@ -584,6 +584,7 @@ private:
 				lowest_layer().close(ec);
 			}
 			change_timer_status(TIMER_DELAY_CLOSE, timer_info::TIMER_CANCELED);
+			unpacker_->reset(); //very important, otherwise, the unpacker will never be able to parse any more messages if its buffer has legacy data
 			on_close();
 			after_close();
 			set_async_calling(false);
