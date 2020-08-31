9515dc1e73d8ddb6eda4c2c62d554c1c3e8fd775
youngwolf <mail2tao@163.com>
2020-08-31 19:52:41 +0800

    Fix bug: shutdown operation causes async_connect invocation to return successful.

--- include/ascs/socket.h
+++ include/ascs/socket.h
@@ -308,7 +308,7 @@ protected:
 #endif
 
 	//subclass notify shutdown event
-	bool close()
+	bool close(bool use_close = false) //if not use_close, shutdown (both direction) will be used
 	{
 		scope_atomic_lock lock(start_atomic);
 		while (!lock.locked())
@@ -328,7 +328,7 @@ protected:
 		if (lowest_layer().is_open())
 		{
 			asio::error_code ec;
-			lowest_layer().shutdown(asio::ip::tcp::socket::shutdown_both, ec);
+			use_close ? lowest_layer().close(ec) : lowest_layer().shutdown(asio::ip::tcp::socket::shutdown_both, ec);
 
 			stat.break_time = time(nullptr);
 		}
