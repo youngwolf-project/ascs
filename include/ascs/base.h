ae4779688951817794f9ba24992794098bce7aef
youngwolf <li.10.yang@nokia.com>
2026-03-31 17:10:38 +0800

    Delete macro ASCS_CAN_EMPTY_NOT_SAFE and ASCS_ARBITRARY_SEND, then your queue must provide 'is_empty' function,  this is to simplify code and guarantee no message left behind in send buffer, see queue::is_empty in container.h for more details.

--- include/ascs/base.h
+++ include/ascs/base.h
@@ -44,7 +44,7 @@ namespace ascs
 class scope_atomic_lock : public boost::noncopyable
 {
 public:
-	scope_atomic_lock(std::atomic_flag& atomic_) : atomic(atomic_) {lock();} //atomic_ must has been initialized as clear state
+	scope_atomic_lock(std::atomic_flag& atomic_) : atomic(atomic_) {lock();} //atomic_ must has been initialized
 	~scope_atomic_lock() {unlock();}
 
 	void lock() {if (!_locked) _locked = !atomic.test_and_set(std::memory_order_acq_rel);}
@@ -570,12 +570,12 @@ template<typename _Predicate> void NAME(const _Predicate& __pred) const {for (au
 }
 
 #define GET_PENDING_MSG_SIZE(FUNNAME, CAN) size_t FUNNAME() const {return CAN.size_in_byte();}
-#define POP_FIRST_PENDING_MSG(FUNNAME, CAN, MSGTYPE) void FUNNAME(MSGTYPE& msg) {msg.clear(); if (CAN.try_dequeue(msg)) _send_msg();}
+#define POP_FIRST_PENDING_MSG(FUNNAME, CAN, MSGTYPE) void FUNNAME(MSGTYPE& msg) {msg.clear(); CAN.try_dequeue(msg);}
 #define POP_FIRST_PENDING_MSG_NOTIFY(FUNNAME, CAN, MSGTYPE) void FUNNAME(MSGTYPE& msg) \
-	{msg.clear(); if (CAN.try_dequeue(msg)) {_send_msg(); if (msg.p) msg.p->set_value(sync_call_result::NOT_APPLICABLE);}}
-#define POP_ALL_PENDING_MSG(FUNNAME, CAN, CANTYPE) void FUNNAME(CANTYPE& can) {can.clear(); CAN.swap(can); _send_msg();}
+	{msg.clear(); if (CAN.try_dequeue(msg)) {if (msg.p) msg.p->set_value(sync_call_result::NOT_APPLICABLE);}}
+#define POP_ALL_PENDING_MSG(FUNNAME, CAN, CANTYPE) void FUNNAME(CANTYPE& can) {can.clear(); CAN.swap(can);}
 #define POP_ALL_PENDING_MSG_NOTIFY(FUNNAME, CAN, CANTYPE) void FUNNAME(CANTYPE& can) \
-	{can.clear(); CAN.swap(can); _send_msg(); ascs::do_something_to_all(can, [](typename CANTYPE::reference msg) {if (msg.p) msg.p->set_value(sync_call_result::NOT_APPLICABLE);});}
+	{can.clear(); CAN.swap(can); ascs::do_something_to_all(can, [](typename CANTYPE::reference msg) {if (msg.p) msg.p->set_value(sync_call_result::NOT_APPLICABLE);});}
 
 ///////////////////////////////////////////////////
 //TCP msg sending interface
